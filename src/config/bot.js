2591794069387150ef2e1e3a99b723556655743d
pluushiez <theyluv.ang3ll@gmail.com>
2026-09-19 21:22:32 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "watching over /myoui hub",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#ffd5f0",
+      secondary: "#fff4d5",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
